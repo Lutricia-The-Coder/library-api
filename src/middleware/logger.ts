4cb897e82f3487326177d9f63753dc1887fc39f9
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-09-18 11:05:37 +0200

    implement get all function

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,7 @@
-import {Request , Response ,NextFunction } from 'express'
+import { Request, Response, NextFunction } from "express";
 
-//logger middleware (helpls debug and monitor the server activity and understand theflow of traffic)
-export const loggerMiddleware = (req: Request , res:Response , next:NextFunction) => {
-    console.log('[$(new Date().toString()] $(req.method) $(req.url)');
-    next()
-}
-
+// Logs the HTTP method and URL of each request.
+export const logger = (req: Request,_res: Response, next: NextFunction): void => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,11 +1,24 @@
-import {Router , Request, Response} from 'express';
-import {body, param, validationResult} from 'express-validator'
+import { Router, Request, Response } from "express";
+import { authors, Author } from "../models/model";
 
-const router= Router()
+const router = Router();
 
-let authors = [
-    {id:1, a_name: "" , title:"", year: 2026},
-     {id:2, a_name: "" , title:"", year: 2026}
-]
+// Create a new author.
+router.post("/", (req: Request, res: Response) => {
+  const { name } = req.body;
 
-router.get("/", [])
+  const author: Author = {
+    id: authors.length + 1,
+    name,
+    dateAdded: new Date().toISOString(),
+  };
+
+  authors.push(author);
+
+  res.status(201).json(author);
+});
+
+// Get all authors.
+router.get("/", (_req: Request, res: Response) => {
+  res.status(200).json(authors);
+});
