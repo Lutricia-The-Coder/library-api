dd29ee966e28fcf23e696f572e2a5512e0525dd1
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-09-18 11:11:45 +0200

    implement get author by id  function

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -22,3 +22,19 @@ router.post("/", (req: Request, res: Response) => {
 router.get("/", (_req: Request, res: Response) => {
   res.status(200).json(authors);
 });
+
+// Get an author by ID.
+router.get("/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  const author = authors.find((author) => author.id === id);
+
+  if (!author) {
+    return res.status(404).json({
+      message: "Author not found",
+    });
+  }
+
+  res.status(200).json(author);
+});
+
